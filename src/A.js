91257fac6295cb3dc3f89d53622da4a99df2c27a
Akbar Basha KM <akbar@animaker.com>
2020-08-20 18:14:31 +0530

    Changed to Class Component

--- src/A.js
+++ src/A.js
@@ -1,1 +1,10 @@
-export default () => 'A'
+import React, { Component } from 'react';
+
+class A extends Component {
+
+    render() {
+        return 'A'
+    }
+}
+
+export default A;
--- src/B.js
+++ src/B.js
@@ -1,1 +1,10 @@
-export default () => 'B'
+import React, { Component } from 'react';
+
+class B extends Component {
+
+    render() {
+        return 'B'
+    }
+}
+
+export default B;
