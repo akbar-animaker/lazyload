9da322bcb02e1c2cef2bea01624f17985cb48b6c
Akbar Basha KM <akbar@animaker.com>
2020-08-20 18:25:05 +0530

    Fixed class component issue

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { Component } from 'react'
 import { render } from 'react-dom'
 import loadable from '@loadable/component'
 
@@ -8,22 +8,39 @@ const Dynamic = loadable(p => import(`./${p.name}`), {
 })
 const Moment = loadable.lib(() => import('moment'))
 
-function App() {
-  const [name, setName] = useState(null)
+class App extends Component {
 
-  return (
-    <div>
-      <button type="button" onClick={() => setName('A')}>
-        Go to A
-      </button>
-      <button type="button" onClick={() => setName('B')}>
-        Go to B
-      </button>
-      {name && <Dynamic name={name} />}
-      <Hello />
-      <Moment>{({ default: moment }) => moment().format('HH:mm')}</Moment>
-    </div>
-  )
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: null
+    }
+  }
+
+  render() {
+    const { name } = this.state;
+    return (
+      <div>
+        <button type="button" onClick={() => {
+          this.setState({
+            name: 'A'
+          })
+        }}>
+          Go to A
+        </button>
+        <button type="button" onClick={() => {
+            this.setState({
+              name: 'B'
+            })
+        }}>
+          Go to B
+        </button>
+        {name && <Dynamic name={name} />}
+        <Hello />
+        <Moment>{({ default: moment }) => moment().format('HH:mm')}</Moment>
+      </div>
+    )
+  }
 }
 
 const root = document.createElement('div')
